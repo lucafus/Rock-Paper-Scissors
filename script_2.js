5714fad8a6b7d59463c14d1b0bee700e800ea77d
lucafus <luca.fusaro1996@gmail.com>
2023-12-12 16:04:15 -0300

    UI in process

--- script_2.js
+++ script_2.js
@@ -1,88 +1,85 @@
+// I create two variables with both score from 0
 
-// By declaring both of these variables with a 0 I can start using them as a part of the score system // 
 
 let playerscore = 0;
 let computerscore = 0;
 
-// If function that will compare the player and computer selection and return a message depending on the result //
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "Tied! Please try again";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerscore++;
-    return "You win!";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerscore++;
-    return "You lost!";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lost!", computerscore++;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerscore++;
-    return "You win!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerscore++;
-    return "You lost!";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerscore++;
-    return "You win!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerscore++;
-    return "You lost!";
-  }
-}
+// By using DOM method is create a variable that alligns the score text from my html with the ones from javascript
+//I do the same with the three buttons, this will be use further in the code
 
-// Function that will select a random value from a set of arrays using the Math resource, this will be used in the playRound function above //
+const playerscoreE = document.querySelector ('#playerscore');
+const computerscoreE = document.querySelector ('#computerscore');
+const result = document.querySelector('#result')
 
-function getComputerChoice() {
+const btnr = document.querySelector('#btnr');
+const btns = document.querySelector('#btns');
+const btnp = document.querySelector('#btnp');
+
+//I create function that will select from an array a random value using the method math.random
+
+function computerSelection() {
   const arr = ["rock", "paper", "scissors"];
-
   const random = Math.floor(Math.random() * arr.length);
-
   return arr[random];
 }
 
-let computerSelection;
-let playerSelection;
+let playerSelection ;
 
-console.log(playRound(playerSelection, computerSelection));
+//The playround function will use the player and computer selection inside an if function. This will check different instnaces of the game
+//I use operands to join the player and computer selection inside one brackets to make the code more clear
+// Each time anyone lose it will add a poin in the score section 
+//This will be shown in the webpage by using the DOM method of .textContent and the previously created variable
 
-// By using a loop function the function is going to keep going until the playerscore until the score is 5 
-// This is going to be set up in an if function that will show a message depending on the result 
+function playRound (playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tied! Please try again";
+  } else if ( (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ) 
 
+  {playerscore++; 
+   playerscoreE.textContent = "Player Score: " + playerscore;
+   return "You win!";}
+  
+  else {computerscore++; 
+        computerscoreE.textContent = "Computer Score: " + computerscore;
+        return "You lost!";
+       
+       }
+}
+
+// If anyone of the scores reach up to 5 it will show a message depending on the result
 
 function gamescore() {
-  if (playerscore >= 5 && computerscore < 5) {
+  if (playerscore >= 5 || computerscore >= 5) {
     console.log("You win the five rounds, congratulations!");
   } else {
     console.log("You lost, too bad!");
   }
 }
 
-// By using a loop function the function is going to keep going until the playerscore until the score is 5,
-// Inside this function called game it I will put the prompt and round variable selection from the player and computer, by doing this inside the loop It will repeat the game until the score is set //
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "What is your selected weapon: "
-    ).toLowerCase();
-    console.log("You choose " + playerSelection);
+//This will add an eventlistener to each of the buttons, inside it a function that will definde the playerSelection value and it will activate
+//The playround with the variable and the computer selection function also to finish it will launch the gamescore. 
+//By doing this it will add the result of each game. 
 
-    if (["rock", "paper", "scissors"].includes(playerSelection)) {
-      const computerSelection = getComputerChoice();
-      console.log("Computer choose " + computerSelection);
-      console.log(playRound(playerSelection, computerSelection));
-    } else {
-      console.log("Invalid Input, please choose rock, paper or scissors.");
-    }
-  }
+btnr.addEventListener('click', () => {
+playerSelection = "rock";
+  playRound( playerSelection ,computerSelection() )
+  gamescore();; } );
+  
+  
+btns.addEventListener('click', () => {
+playerSelection = "scissors";
+    playRound( playerSelection ,computerSelection() )
+  gamescore();; } );
 
-// I will launch the gamescore inside the game function to log in all of the result and show the message depending on the result  
 
-  gamescore();
-}
+btnp.addEventListener('click', () => {
+playerSelection = "paper";
+    playRound(playerSelection ,computerSelection() ); 
+gamescore();} );
 
-// This will launch the function game and start the process
 
-game();
+
